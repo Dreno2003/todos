2dbb671b3ba471d639f8e34161b1257681b9cba0
Idorenyin Akaininyene <=>
2022-08-08 03:56:56 +0100

    p

--- src/firebase-script.js
+++ src/firebase-script.js
@@ -129,6 +129,61 @@ export const createUserDocInFirestore = (email) => {
     
 // signInWithFacebookButton.addEventListener('click', signInWithFacebook);
 
+function signIn() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase
+  .auth()
+  .signInWithPopup(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+      location.href = "/todos.html";
+      
+
+    // The signed-in user info.
+    var user = result.user;
+
+    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+    var accessToken = credential.accessToken;
+
+    // ...
+  })
+  .catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+
+    // ...
+  });
+    
+  firebase.auth()
+  .getRedirectResult()
+  .then((result) => {
+    if (result.credential) {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = credential.accessToken;
+      // ...
+    }
+    // The signed-in user info.
+    var user = result.user;
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
 
 
 
