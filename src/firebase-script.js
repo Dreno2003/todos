ba70ba99c39cfc1941559f457555d89f3a0e55cb
Idorenyin Akaininyene <=>
2022-08-08 01:55:36 +0100

    p

--- src/firebase-script.js
+++ src/firebase-script.js
@@ -120,34 +120,25 @@ export const createUserDocInFirestore = (email) => {
 
 // const auth = firebase.auth();
 
-var provider = new firebase.auth.FacebookAuthProvider();
+// var provider = new firebase.auth.FacebookAuthProvider();const signInWithFacebook = () => {
+    const signInWithFacebook = () => {
+        const facebookProvider = new firebase.auth.FacebookAuthProvider();
+      
+        auth.signInWithPopup(facebookProvider)
+        .then(() => {
+          window.location.assign('./profile');
+        })
+        .catch(error => {
+          console.error(error);
+        })
+      }
+      
+      signInWithFacebookButton.addEventListener('click', signInWithFacebook);
+// signInWithFacebookButton.addEventListener('click', signInWithFacebook);
 
-firebase
-  .auth()
-  .signInWithPopup(provider)
-  .then((result) => {
-    /** @type {firebase.auth.OAuthCredential} */
-    var credential = result.credential;
 
-    // The signed-in user info.
-    var user = result.user;
 
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-    var accessToken = credential.accessToken;
 
-    // ...
-  })
-  .catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-
-    // ...
-  });
 
 // sign up a user with email and pwd
 export const signUpUser = (displayName, email, password) => {
